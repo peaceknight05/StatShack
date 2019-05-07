ccc70536aaaead05cf4cd3f1f9b6f77c3fbaa3db
Santhiyaa Senthilkumar <santhiyaa@santhiyaas-macbook-pro.local>
2019-05-07 10:21:54 +0800

    Initial Commit

---
+++ StatShack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StatShack
+//
+//  Created by Santhiyaa Senthilkumar on 7/5/19.
+//  Copyright © 2019 Santhiyaa Senthilkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
