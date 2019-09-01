8efb22efcb6ff207ec1b515c237b965ca88895bc
Jonathan Tan Jiayi <jonathantanatlol@gmail.com>
2019-09-01 16:04:02 +0800

    Revert "fixed clock thing"
    This reverts commit dd3a9c8299614b9d10addcaa65fd6f97deb749cf.

--- StatShack/HandballScoreCounterVC.swift
+++ StatShack/HandballScoreCounterVC.swift
@@ -10,75 +10,25 @@ import UIKit
 
 class HandballScoreCounterVC: UIViewController {
     // MARK: var
-
-    @IBOutlet var mainClockLabel: UILabel!
     var startTime: Date?
     var elapsedTime: TimeInterval {
         if let startTime = self.startTime {
-            return -startTime.timeIntervalSinceNow - pausedSeconds
+            return -startTime.timeIntervalSinceNow
         } else {
             return 0
         }
     }
-
-    var timer: Timer?
-    var isPaused = false
-    var pausedTime: Date?
-    var pausedIntervals: [TimeInterval] = []
-    var pausedSeconds: Double {
-        var a = pausedIntervals.reduce(0) { $0 + $1 }
-        if let pausedTime = pausedTime {
-            a += -pausedTime.timeIntervalSinceNow
-        }
-        return a
-    }
-
-    var elapsedTimeAsString: String {
-        return String(format: "%02d:%02d", Int(elapsedTime / 60), Int(elapsedTime.truncatingRemainder(dividingBy: TimeInterval(60))))
-    }
-
-    var isRunning = false
     var teamOneNames: [Positions: String]?
     var teamTwoNames: [Positions: String]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
     func startTimer() {
-        pausedIntervals.append(pausedSeconds)
-        pausedTime = nil
-
-        isPaused = false
-        isRunning = true
-    }
-
-    func stopTimer() {
-        guard isRunning && !isPaused else {
-            return
-        }
-        timer?.invalidate()
-        isPaused = true
-        isRunning = false
-        pausedTime = Date()
-    }
-
-    @objc func updateElapsedTimeLabel() {
-        if isRunning {
-            mainClockLabel.text = elapsedTimeAsString
-        }
-    }
-
-    @IBAction func startStop(_ sender: Any) {
-        if !isRunning {
-            if timer == nil {
-                timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateElapsedTimeLabel), userInfo: nil, repeats: true)
-                startTime = Date()
-            }
-            startTimer()
-        } else {
-            stopTimer()
-        }
+        startTime = Date()
     }
 }
 
